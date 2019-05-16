3dfba142203ee53b159d8a0945cde2e9b4e7dd38
Leandrorferreira <leandrorferreira95@gmail.com>
2019-05-16 08:53:13 -0300

    Criar App Estabalecimento

---
+++ establishment/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Establishment(models.Model):
+    es_name = models.CharField(max_length=255)
+    es_site =  models.CharField(max_length=100)
+    es_phone = models.CharField(max_length=11)
+    es_cnpj = models.CharField(max_length=14)
+    es_street = models.CharField(max_length=100)
+    es_zip_code = models.CharField(max_length=8)
+    es_state = models.CharField(max_length=2)
+    es_city = models.CharField(max_length=100)
+    es_number = models.IntegerField()
