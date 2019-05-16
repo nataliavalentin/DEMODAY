82fea3c7522539eda9e16ece8d7faaa17f659e97
Leandrorferreira <leandrorferreira95@gmail.com>
2019-05-16 16:44:24 -0300

    Criar Model de Estabelecimento

--- establishment/models.py
+++ establishment/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Establishment(models.Model):
-    es_name = models.CharField(max_length=255)
+    es_name = models.CharField(max_length=255)    
+    es_rating = models.IntegerField(default=0, null=True)
     es_site =  models.CharField(max_length=100)
     es_phone = models.CharField(max_length=11)
     es_cnpj = models.CharField(max_length=14)
@@ -11,3 +12,6 @@ class Establishment(models.Model):
     es_state = models.CharField(max_length=2)
     es_city = models.CharField(max_length=100)
     es_number = models.IntegerField()
+
+    def __str__(self):
+        return self.es_name
