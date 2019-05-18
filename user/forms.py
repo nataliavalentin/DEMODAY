fdb24ff7f3a304c6e33ed897fc912c8584ec74bf
Nathália Spinula <nathaliaspinula@gmail.com>
2019-05-18 17:54:01 -0300

    Alterar

--- user/forms.py
+++ user/forms.py
@@ -1,26 +1,6 @@
 from django import forms
 from user.models import User
 from django.contrib.auth.forms import UserCreationForm
-
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields =[
-            'user_name',
-            'user_last_name',
-            'user_email',
-            'user_cellphone',
-            'user_birthday'
-        ]
-
-    def __init__(self, *args, **kwargs):
-        super(UserForm, self).__init__(*args, **kwargs)
-        self.fields['user_name'].label = "Nome"
-        self.fields['user_last_name'].label = "Sobrenome"
-        self.fields['user_email'].label = "E-mail"
-        self.fields['user_cellphone'].label = "Celular"
-        self.fields['user_birthday'].label = "Data de Nascimento"
-
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(label='Nome', max_length=255)
     last_name = forms.CharField(label='Sobrenome', max_length=255)
--- user/views.py
+++ user/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from user.forms import UserForm
 from user.models import User
 from django.conf import settings
 from .forms import RegisterForm
@@ -14,20 +13,6 @@ def login(request):
 def dashboard(request):
     template_name = 'user/dashboard.html'
     return render(request, template_name)
-    
-def register(request):
-    formulario = UserForm(request.POST or None)
-    msg = ''
-    if formulario.is_valid():
-        formulario.save()
-        formulario = UserForm()
-        msg = 'Cadastro realizado com sucesso!'
-    
-    contexto = {
-        'form' : formulario,
-        'msg' : msg
-    }
-    return render(request, 'register.html', contexto)
 
 def registerUser(request):
     template_name = 'reg.html'
