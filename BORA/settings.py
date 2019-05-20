83524e5225376577228fade1fabdc4568f5ed679
nataliavalentin <nataliaferreiravalentin@gmail.com>
2019-05-20 11:20:21 -0300

    Adicionar configurações do hiroku

--- BORA/settings.py
+++ BORA/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,3 +151,5 @@ EMAIL_HOST_PASSWORD = 'LMNNV@00'
 EMAIL_PORT = 587
 
 CONTACT_EMAIL = 'projetobora.contato@gmail.com'
+
+django_heroku.settings(locals())
